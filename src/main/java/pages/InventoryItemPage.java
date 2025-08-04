af88a275069a28ec3d23d02b01de47288276eeda
Ravi Sampathirao <ravi.sampathirao@gspann.com>
2025-08-04 18:03:19 +0530

    Add inventry.feature and inventry iteampage

--- src/main/java/pages/InventoryItemPage.java
+++ src/main/java/pages/InventoryItemPage.java
@@ -5,5 +5,38 @@ import driver.WebDriverFactory;
 
 public class InventoryItemPage {
 
-	
+	private By itemImage = By.className("inventory_details_img");
+	private By itemName = By.className("inventory_details_name");
+	private By itemDescription = By.className("inventory_details_desc");
+	private By itemPrice = By.className("inventory_details_price");
+	private By addToCartButton = By.cssSelector("button.btn_inventory");
+	private By backButton = By.id("back-to-products");
+
+	public String getItemName() {
+		return WebDriverFactory.getDriver().findElement(itemName).getText();
+	}
+
+	public String getItemDescription() {
+		return WebDriverFactory.getDriver().findElement(itemDescription).getText();
+	}
+
+	public String getItemPrice() {
+		return WebDriverFactory.getDriver().findElement(itemPrice).getText();
+	}
+
+	public String getItemImageSrc() {
+		return WebDriverFactory.getDriver().findElement(itemImage).getAttribute("src");
+	}
+
+	public void clickAddToCart() {
+		WebDriverFactory.getDriver().findElement(addToCartButton).click();
+	}
+
+	public void clickBackToProducts() {
+		WebDriverFactory.getDriver().findElement(backButton).click();
+	}
+
+	public void meanuButton() {
+
+	}
 }
